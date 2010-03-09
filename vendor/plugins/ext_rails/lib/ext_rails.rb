02c869dab18aba19f20a4a7bc482ee02bf715c08
fsword <li.jianye@gmail.com>
2010-03-09 22:35:34 +0800

    去除ext_rails代码，准备使用git管理这个插件

--- vendor/plugins/ext_rails/lib/ext_rails.rb
+++
@@ -1,6 +0,0 @@
-%W[action_view].each do |mod|
-  Dir[File.dirname(__FILE__) + "/#{mod}/*.rb"].each{|file|
-    RAILS_DEFAULT_LOGGER.info "#{mod} is patched by ext_rails"
-    require file
-  }
-end
--- vendor/plugins/ext_rails/tasks/ext_rails_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :ext_rails do
-#   # Task goes here
-# end
