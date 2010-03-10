3c8907ba1da775c6526173f3e6adadbec0add149
fsword <li.jianye@gmail.com>
2010-03-10 15:52:35 +0800

    尝试加入link支持（后续需要建立link模型）

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,5 +1,5 @@
 class Network < ActiveRecord::Base
-#  belongs_to :master, :class_name => 'Member'
-#  has_many :publishers, :class_name => 'PublisherInfo'
-#  has_many :advertisers, :class_name => "AdvertiserInfo"
+  belongs_to :master, :class_name => 'Member'
+  has_many :publishers, :class_name => 'PublisherInfo'
+  has_many :advertisers, :class_name => "AdvertiserInfo"
 end
--- vendor/plugins/box_scaffold/lib/box_scaffold/config/core.rb
+++ vendor/plugins/box_scaffold/lib/box_scaffold/config/core.rb
@@ -14,10 +14,6 @@ module BoxScaffold::Config
       @clazz ||= @model_id.classify.constantize
     end
     
-    def new_path
-      @new_path ||= {:action => :new, :controller => @model_id.tableize}
-    end
-
     def model_id
       @model_id
     end
@@ -25,6 +21,23 @@ module BoxScaffold::Config
     def model
       @model ||= @model_id.to_s.camelize.constantize
     end
+
+     def links
+       [
+         {:label => 'show', :url => path_template(:show) },
+         {:label => 'edit', :url => path_template(:edit) },
+         {:label => 'destroy', :url => path_template(:destroy)}
+       ]
+     end
+
+    def new_path
+      @new_path ||= path_template(:new)
+    end
+
+    def path_template action
+      {:action => action.to_s, :controller => @model_id.tableize}
+    end
+
   end
 
 end
